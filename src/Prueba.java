c8d82eed9f6bfcc842273fe71c95648f2213ae39
coderpug <torrescardenasjose@gmail.com>
2021-11-27 12:03:33 -0500

    Extender Prueba para soportar 3 parametros

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,13 +2,15 @@ public class Prueba {
 
     private int atributo1;
     private int atributo2;
+    private int atributo3;
 
-    public Prueba(int atributo1, int atributo2) {
+    public Prueba(int atributo1, int atributo2, int atributo3) {
         this.atributo1 = atributo1;
         this.atributo2 = atributo2;
+        this.atributo3 = atributo3;
     }
 
     public int sumar() {
-        return atributo1 + atributo2;
+        return atributo1 + atributo2 + atributo3;
     }
 }
