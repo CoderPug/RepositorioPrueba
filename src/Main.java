b8d7097c9ae3a908f96dfc0ca9f53ef7020ea29a
coderpug <torrescardenasjose@gmail.com>
2021-11-28 12:05:31 -0500

    Eliminar soporte de atributos de texto

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Prueba prueba = new Prueba(1, 2, 3, "Hola", " ", "Mundo");
+        Prueba prueba = new Prueba(1, 2, 3);
 
         System.out.println("Sumar:");
         System.out.println("=====");
@@ -13,9 +13,8 @@ public class Main {
         System.out.println(prueba.restar());
 
         System.out.println("Multiplicar:");
+        System.out.println("=====");
         System.out.println(prueba.multiplicar());
 
-        System.out.println("Concatenar:");
-        System.out.println(prueba.concatenar());
     }
 }
--- src/Prueba.java
+++ src/Prueba.java
@@ -4,25 +4,14 @@ public class Prueba {
     private int atributo2;
     private int atributo3;
 
-    private String atributo4;
-    private String atributo5;
-    private String atributo6;
-
-    public Prueba(int atributo1, int atributo2, int atributo3, String atributo4, String atributo5, String atributo6) {
+    public Prueba(int atributo1, int atributo2, int atributo3) {
         this.atributo1 = atributo1;
         this.atributo2 = atributo2;
         this.atributo3 = atributo3;
-        this.atributo4 = atributo4;
-        this.atributo5 = atributo5;
-        this.atributo6 = atributo6;
     }
 
     public int sumar() {
         return atributo1 + atributo2 + atributo3;
-    }
-
-    public String concatenar() {
-        return atributo4 + atributo5 + atributo6;
     }
 
     public int restar() {
